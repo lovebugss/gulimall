9245270e3e94d5caafa486d6a4f03d30561ede63
renjp <979668507@qq.com>
2020-08-01 16:45:08 +0800

    project init

---
+++ gulimall-product/src/test/java/com/itrjp/gulimaill/product/GulimaillProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itrjp.gulimaill.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimaillProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-ware/src/test/java/com/itrjp/gilimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itrjp.gilimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
